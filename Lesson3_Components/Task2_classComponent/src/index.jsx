b2844c9efa744a0f96be3c8f606ec2cd25f0bb8d
vvspl <vladpolt@gmail.com>
2021-07-06 23:29:45 +0300

    task

---
+++ Lesson3_Components/Task2_classComponent/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from './Search.jsx';
+import './search.scss';
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Search name="Tom" />, rootElement);
