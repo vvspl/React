9f434956a6f7e29e99e7dcf9da582c5ee13bea77
vvspl <vladpolt@gmail.com>
2021-07-07 18:11:24 +0300

    task

--- Lesson3_Components/Task3_ComponentMix/src/App.jsx
+++ Lesson3_Components/Task3_ComponentMix/src/App.jsx
@@ -1,13 +1,19 @@
+
 import React from 'react';
 import Comment from './Comment.jsx';
 
-const userInfo = {
+const UserInfo = {
   name: 'Tom',
-  avatarUrl: 'https://avatars1.githubusercontent.com/avatar_url',
+  avatarUrl: 'https://avatars1.githubusercontent.com',
 };
 
-const App = () => {
-  return <Comment author={userInfo} text="Good job!" date={new Date()} />;
-};
+const App = () => (
+
+    <Comment
+      author={UserInfo}
+      text='Good job!'
+      date={new Date('2019-01-01T11:32:19.566Z')}
+/>
+);
 
 export default App;
--- Lesson3_Components/Task3_ComponentMix/src/Avatar.jsx
+++ Lesson3_Components/Task3_ComponentMix/src/Avatar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './avatar.scss';
 
 const Avatar = props => {
-  return <img className="avatar" src={props.avatarUrl} alt={props.name} />;
+  return <img className='avatar' src={props.avatarUrl} alt={props.name} />;
 };
 
 export default Avatar;
