317a6812bfe97e971bb7f669aaa27ac848f15fb0
vvspl <vladpolt@gmail.com>
2021-07-08 22:56:16 +0300

    task

---
+++ Lesson5_Events/Task1_BtnClick/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+import GoodButton from './GoodButton.jsx';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<GoodButton />, rootElement);
+
