b460166ef48991ca795a2fd679b5aa3860f8cf14
vvspl <vladpolt@gmail.com>
2021-07-10 17:51:18 +0300

    task

---
+++ Lesson6_Render/Task3_RenderCansel/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import Element from './Element.jsx'
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Element />, rootElement);
