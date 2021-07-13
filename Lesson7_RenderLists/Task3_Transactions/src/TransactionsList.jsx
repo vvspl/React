f38226b19d103dbbb70b6f1347d9f13c6510a704
vvspl <vladpolt@gmail.com>
2021-07-13 17:58:24 +0300

    ефыл

---
+++ Lesson7_RenderLists/Task3_Transactions/src/TransactionsList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Transaction from './Transaction.jsx';
+
+const TransactionsList = ({transactions}) => {
+    console.log(transactions);
+  return (
+    <ul className="transactions">
+      {transactions.map(transaction => (
+        <Transaction key={transaction.id} {...transaction} />
+      ))}
+    </ul>
+  );
+};
+
+export default TransactionsList;
