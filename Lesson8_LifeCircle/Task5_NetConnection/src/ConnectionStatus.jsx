231496e2c4a27014f34f5c269325261af25c3dea
vvspl <vladpolt@gmail.com>
2021-07-15 16:25:58 +0300

    task

---
+++ Lesson8_LifeCircle/Task5_NetConnection/src/ConnectionStatus.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class ConnectionStatus extends React.Component{
+    state = {
+        onlineStatus: true,
+    }
+
+componentDidMount(){
+    // if (navigator.onLine) console.log('yes');  // option2
+    window.addEventListener('online', this.onConnected);
+    window.addEventListener('offline', this.onDisconnected);
+};
+
+componentWillUnmount() {
+    window.removeEventListener('online', this.onConnected);
+    window.removeEventListener('offline', this.onDisconnected);
+  }
+
+
+onConnected(){
+    this.setState({
+        onlineStatus: true,
+    });
+}
+
+onDisconnected(){
+    this.setState({
+        onlineStatus: false,
+    });
+}
+
+    render(){
+        if (this.state.onlineStatus) return (<div className="status">Online</div>);
+        return (<div className="status status_offline">Offline</div>);
+    }
+}
+
+export default ConnectionStatus;
