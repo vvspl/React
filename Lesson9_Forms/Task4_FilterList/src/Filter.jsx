a664223067fd293295032b83431df8e611603e15
vvspl <vladpolt@gmail.com>
2021-07-16 16:59:34 +0300

    task

--- Lesson9_Forms/Task4_FilterList/src/Filter.jsx
+++ Lesson9_Forms/Task4_FilterList/src/Filter.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React from 'react';
 
-const Filter = (props) =>{
-    // console.log('FilterProps: ', props.name, props.user);
-    let count = 0;
-    if (props.name)  props.user.forEach(el=>{if (el.name.toUpperCase()===props.name.toUpperCase()) count++; });
-    console.log('CountArr:', count);
-    return (
-        <>
-        </>
-    );
-}
+const Filter = props => {
+  // console.log('FilterProps: ', props.name, props.user, props.onChange);
+  let count = 0;
+  if (props.name)
+    props.user.forEach(el => {
+      if (el.name.toUpperCase() === props.name.toUpperCase()) count++;
+    });
+  console.log('CountArr:', count);
+  return (
+    <div className="filter">
+      <span className="filter__count">{count}</span>
+      <input type="text" className="filter__input" value={props.name} onChange={props.onChange} />
+    </div>
+  );
+};
 export default Filter;
--- Lesson9_Forms/Task4_FilterList/src/UsersList.jsx
+++ Lesson9_Forms/Task4_FilterList/src/UsersList.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import User from './User.jsx';
-import Filter from './Filter.jsx'
+import Filter from './Filter.jsx';
 
 class UsersList extends React.Component {
   state = {
     name: '',
   };
 
-  onChange = event => {
+  onInputChange = event => {
     this.setState({
       name: event.target.value,
     });
   };
 
   render() {
-    console.log(this.props.users);
+    // console.log(this.props.users);
+    // console.log('StateName:', this.state.name);
+    // console.log('output:',this.props.users.filter(f=>f.name===this.state.name));
+    let displayUsers = this.props.users.filter(
+      f => f.name.toUpperCase() === this.state.name.toUpperCase(),
+    );
+    if (displayUsers.length < 1) displayUsers = this.props.users;
     return (
       <div>
-        <Filter name={this.state.name} user ={this.props.users}/>
-        <div className="filter">
-          <span className="filter__count">5</span>
-          <input type="text" className="filter__input" value={this.state.name} onChange={this.onChange} />
-        </div>
+        <Filter name={this.state.name} user={this.props.users} onChange={this.onInputChange} />
         <ul className="users">
-          {this.props.users.map(user => (
-            <User key={user.id} {...user} />
+          {displayUsers.map(user => (
+            <User key={user.id} name={user.name} age={user.age} />
           ))}
         </ul>
       </div>
