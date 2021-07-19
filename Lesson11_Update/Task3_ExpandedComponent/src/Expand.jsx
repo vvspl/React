a71a69350cf35d0dfcb1ccbb9e5d3d383426ac5f
vvspl <vladpolt@gmail.com>
2021-07-19 15:46:15 +0300

    task

--- Lesson11_Update/Task3_ExpandedComponent/src/Expand.jsx
+++ Lesson11_Update/Task3_ExpandedComponent/src/Expand.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
 const Expand = ({ isOpen, children, title, onClose, onOpen }) => {
-  console.log('isOpen: ', isOpen);
-
   return (
     <div className="expand border">
       <div className="expand__header">
         <span className="expand__title">{title}</span>
         <button className="expand__toggle-btn">
-          {!isOpen ? (
-            <i className="fas fa-chevron-down" onClick={onOpen}></i>
-          ) : (
+          {isOpen ? (
             <i className="fas fa-chevron-up" onClick={onClose}></i>
+          ) : (
+            <i className="fas fa-chevron-down" onClick={onOpen}></i>
           )}
         </button>
       </div>
