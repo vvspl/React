08ad64d731879e548faa3bfce88bf096db3566f5
vvspl <vladpolt@gmail.com>
2021-07-19 15:37:21 +0300

    task

---
+++ Lesson11_Update/Task3_ExpandedComponent/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Expand from './Expand';
+
+class App extends React.Component {
+  state = {
+    isOpen: false,
+  };
+
+  hideDialog = () => {
+    this.setState({
+      isOpen: false,
+    });
+  };
+
+  showDialog = () => {
+    this.setState({
+      isOpen: true,
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <Expand isOpen={this.state.isOpen} onClose={this.hideDialog} onOpen={this.showDialog} title="Some title">
+          <p>
+            Hooks are a new addition in React 16.8. They let you use state and other React features
+            without writing a class.
+          </p>
+        </Expand>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Lesson11_Update/Task3_ExpandedComponent/src/Expand.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Expand = ({ isOpen, children, title, onClose, onOpen }) => {
+  console.log('isOpen: ', isOpen);
+
+  return (
+    <div className="expand border">
+      <div className="expand__header">
+        <span className="expand__title">Some title</span>
+        <button className="expand__toggle-btn">
+          {!isOpen ? (
+            <i className="fas fa-chevron-down" onClick={onOpen}></i>
+          ) : (
+            <i className="fas fa-chevron-up" onClick={onClose}></i>
+          )}
+        </button>
+      </div>
+      {isOpen ? <div className="expand__content">{children}</div> : <></>}
+    </div>
+  );
+};
+
+export default Expand;
