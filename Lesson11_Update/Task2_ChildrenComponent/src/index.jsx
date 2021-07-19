9e5cb6428ce7c2714bdce1c67d360f1ef79fb9bd
vvspl <vladpolt@gmail.com>
2021-07-19 11:01:53 +0300

    task

--- Lesson11_Update/Task2_ChildrenComponent/src/index.jsx
+++ Lesson11_Update/Task2_ChildrenComponent/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Element from './Element.jsx'
+import App from './App.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Element />, rootElement);
+ReactDOM.render(<App />, rootElement);
